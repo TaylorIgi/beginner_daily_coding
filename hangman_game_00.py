4fa5b9bd660a25308bfd0fe271315a48d01a685d
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-12 19:50:44 -0300

    Inclusão das funções try_choosen_word e play_game . Definição inicial da função main.

--- hangman_game_00.py
+++ hangman_game_00.py
@@ -34,41 +34,98 @@ def random_word(language="Any", difficulty="Any"):
     return choosen_word
 
 def check_new_letter(user_past_letters, user_letter):
-    for i in range(0, len(user_letter)-1):
+
+    for i in range(0, len(user_past_letters)):
         if user_past_letters[i] == user_letter:
             return "nok"
     return "ok"
 
 def check_letter_in_word(choosen_word, user_word, user_letter):
+    
     new_user_word = user_word
     for i in range(0, len(user_word)):
-        print(f"i = {i} word_letter = {choosen_word[i]} user_letter = {user_letter}")
         if choosen_word[i] == user_letter:
             new_user_word[i] = user_letter
     return new_user_word
 
-# ------------------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------
-# Testes 
-# ------------------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------
+def try_choosen_word(language = "Any", difficulty = "Any"):
+    
+    try_again = True
+    while try_again:
+        try:
+            choosen_word = random_word(language, difficulty)
+            try_again = False
+        except:
+            continue
+    return choosen_word
 
-# choosen_word = random_word()
-choosen_word = {"word": "arara", "language": "Portuguese", "difficulty": "Easy", "word_index": 0, "word_lenght": 5}
+def play_game(choosen_word):
 
-user_word = []
-for i in range(0, len(choosen_word["word"])):
-    user_word.append("_")
-user_past_letters = []
+    import os
 
-user_letter = input("What letter is your guess? ").lower()
+    user_word = []
+    for i in range(0, len(choosen_word["word"])):
+        user_word.append("_")
+    user_past_letters = []
+    remain_guesses = 6
 
-while check_new_letter(user_past_letters, user_letter) == "nok":
-    user_letter = input(f"{user_letter.upper()} has already been guessed! Please guess another letter: ".lower)
+    while ''.join(map(str, user_word)) != choosen_word["word"] and remain_guesses > 0:
 
-user_past_letters.append(user_letter)
-user_word = check_letter_in_word(choosen_word["word"], user_word, user_letter)
+        os.system("cls")
 
-print(choosen_word)
-print(user_word)
-print(user_past_letters)
+        print(f"You can guess wrongly {remain_guesses} more times!\n")
+
+        user_word_str = ''.join(map(str, user_word))
+        print(f"Current word: {user_word_str}")
+
+        if len(user_past_letters) > 0:
+            print(f"Past guesses: {user_past_letters}\n")
+
+        user_letter = input("What letter is your guess? ").lower()
+
+        while check_new_letter(user_past_letters, user_letter) == "nok":
+            user_letter = input(f"{user_letter.upper()} has already been guessed! Please try another letter: ".lower())
+
+        user_past_letters.append(user_letter)
+
+        user_word_aux = []
+        for i in range(0, len(user_word)):
+            user_word_aux.append(user_word[i])
+
+        user_word = check_letter_in_word(choosen_word["word"], user_word, user_letter)
+
+        if user_word == user_word_aux:
+            remain_guesses = remain_guesses - 1
+    
+    if ''.join(map(str,user_word)) == choosen_word["word"]:
+        return "win"
+    else:
+        return "lost"
+
+
+def main():
+
+    import time
+    import os
+
+    choosen_word = try_choosen_word()
+    print(choosen_word["word"])
+    time.sleep(4)
+
+    result = play_game(choosen_word)
+
+    if result == "win":
+        os.system("cls")
+        print(f"\nCongrats!!! You guessed {choosen_word["word"].upper()} correctly!!!\n\n")
+    elif result == "lost":
+        os.system("cls")
+        print(f"\nToo bad...You lost...")
+        print(f"The word was {choosen_word['word'].upper()}\n\n")
+    else:
+        os.system("cls")
+        print("\nSorry...we had some problem...\n\n")
+
+    if input("Do you wanna play again? (Yes or No)\n").lower() == "yes":
+        main()
+
+main()
