23fecbc010d7003690fed0fd051f02296819fb6b
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-12 17:01:52 -0300

    Funcao que escolhe a palavra passa a retornar um dict ao inves de só a palavra

--- hangman_game_00.py
+++ hangman_game_00.py
@@ -13,7 +13,7 @@ def random_word(language="Any", difficulty="Any"):
 
     if ( language != "Any" ) and ( difficulty != "Any" ):
         df_aux = df_words.loc[df_words["language"] == language]
-        df_filtered = df_aux.loc[df_words["difficulty"] == difficulty]
+        df_filtered = df_aux.loc[df_aux["difficulty"] == difficulty]
     elif language != "Any":
         df_filtered = df_words.loc[df_words["language"] == language]
     elif difficulty != "Any":
@@ -22,9 +22,15 @@ def random_word(language="Any", difficulty="Any"):
         df_filtered = df_words
     
     my_index = rd.randint(0, df_filtered.index.max())
-    print(my_index)
-    print(df_filtered.loc[my_index, "word"])
-    print(df_filtered.loc[my_index, "language"])
-    print(df_filtered.loc[my_index, "difficulty"])
 
-random_word("Portuguese", "Hard")
+    choosen_word = {
+        "word": df_filtered.loc[my_index, "word"],
+        "language": df_filtered.loc[my_index, "language"],
+        "difficulty": df_filtered.loc[my_index, "difficulty"],
+        "word_index": my_index
+    }
+
+    return choosen_word
+
+word = random_word("Portuguese", "Hard")
+print(word)
