088d4b5da4b422dadd281a9c0b2350ffbce604f5
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-14 15:08:29 -0300

    Funções principais testadas

--- Currency_Converter/readme.md
+++ Currency_Converter/readme.md
@@ -2,3 +2,5 @@ __Goal:__ create a Python program to convert currencies using API
 __Inspiration:__ Chat GPT
 
 This program uses informations from https://fixer.io/
+
+The free version of this API suports only EUR as currency base
--- Currency_Converter/utils.py
+++ Currency_Converter/utils.py
@@ -3,9 +3,6 @@ import pandas as pd
 
 main_url = "http://data.fixer.io/api/latest"
 access_key = "38c5db7cd2251ee9ee082d6d60b2fa35"
-base = "EUR"
-
-api_url = f"{main_url}?access_key={access_key}&base={base}"
 
 def api_get_data(url_path):
     response = requests.get(url_path)
@@ -19,7 +16,18 @@ def check_api_get_data(response_sts_code):
 def from_api_get_to_dataframe(my_object):
     return pd.DataFrame(my_object.json())
 
-response = api_get_data(api_url)
+def get_eur_rate(currency_code_from, currency_code_to):
+    api_url = f"{main_url}?access_key={access_key}&symbols={currency_code_from}, {currency_code_to}"
+    response = api_get_data(api_url)
+    if not check_api_get_data:
+        return -1
+    df_currency = from_api_get_to_dataframe(response)
+    rates = []
+    for row in range(0, len(df_currency)):
+        rates.append({"currency_name": df_currency.iloc[row].name, "currency_rate": df_currency.iloc[row]["rates"]})
+    return rates
 
-df = from_api_get_to_dataframe(response)
-print(df.head(5))
+def converter(value, currency_code_from, currency_code_to, rates):
+    if rates[0]["currency_name"] == currency_code_from:
+        return value * rates[1]["currency_rate"] / rates[0]["currency_rate"]
+    return value * rates[0]["currency_rate"] / rates[1]["currency_rate"]
