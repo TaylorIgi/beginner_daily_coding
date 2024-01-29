853872b5302bd49aeffe84768540f3cef0b7d9f3
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-29 19:17:24 -0300

    Testes de pegar nome da cidade, cadastrar no token e pegar os dados do clima

--- Weather_Forecast/tests.py
+++ Weather_Forecast/tests.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import requests
 
-api_main_url = "http://apiadvisor.climatempo.com.br/api/v1/locale/city?"
+api_main_city_url = "http://apiadvisor.climatempo.com.br/api/v1/locale/city?"
+api_main_weather_url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/"
+# api_main_weather_url = "http://apiadvisor.climatempo.com.br/api/v1/climate/temperature/locale/"
+# api_main_weather_url = "http://apiadvisor.climatempo.com.br/api/v1/climate/rain/locale/"
+api_suffix_weather_url = "/current?token="
 my_token = "1766b03ce7ea5fbb097b15f4ef065905"
 my_country = ""
 my_name = "São P"
@@ -35,13 +39,45 @@ def check_api_get_data(response_sts_code):
     return False
 
 # Transforma os dados da API em um dataframe
-def from_api_get_to_dataframe(my_object):
-    return pd.DataFrame(my_object.json())
+def from_api_get_to_dataframe(api_response):
+    return pd.DataFrame(api_response.json())
 
-api_response = api_get_data(get_full_url(api_main_url, my_token, my_name, my_state))
+# api_response = api_get_data(get_full_url(api_main_city_url, my_token, my_name, my_state))
 
+# if not check_api_get_data(api_response.status_code):
+#      print("Erro ao carregar os dados da API")
+#      exit
+
+# df_cities = from_api_get_to_dataframe(api_response)
+
+# choosen_city_index = int(input("Digite o número da cidade: "))
+# choosen_city_id = df_cities.loc[choosen_city_index, "id"]
+# choosen_city_name = df_cities.loc[choosen_city_index, "name"]
+# choosen_city_state = df_cities.loc[choosen_city_index, "state"]
+
+'''   ----------------------------------------------------   '''
+
+import requests
+
+url = f"http://apiadvisor.climatempo.com.br/api-manager/user-token/{my_token}/locales"
+choosen_city_id = 3477
+
+payload = {'localeId[]': choosen_city_id}
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+response_register_city = requests.put(url.replace(':your-app-token', my_token), data=payload, headers=headers)
+
+'''   ----------------------------------------------------   '''
+
+api_response = api_get_data(api_main_weather_url + str(choosen_city_id) + api_suffix_weather_url + my_token)
+print(api_main_weather_url + str(choosen_city_id) + api_suffix_weather_url + my_token)
 if not check_api_get_data(api_response.status_code):
      print("Erro ao carregar os dados da API")
      exit
 
-print(from_api_get_to_dataframe(api_response).head(10))
+print(api_response)
+print(api_response.text)
+
+# df_weather = from_api_get_to_dataframe(api_response)
+
+# print(df_weather.head(10))
